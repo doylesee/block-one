23df2086900d1ebf09b1a05f2b48ed2bff7a66c7
doylesee <doyle@mybizniche.com>
2015-10-24 17:08:49 -0700

    Initial files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).foundation();
+
+$(document).ready(function(){
+	// Landing page: Banner section
+	$('.banner__list').slick({
+			arrows		: false,
+			dots		: true,
+			fade		: true,
+			autoplay	: true,
+			speed		: 300,
+			infinite	: true
+	});
+	
+});
+
+$(window).load(function() {
+	
+});
